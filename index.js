d68abedae23f94387417bc7f4a2ffc89fd01b14c
ofirlevy6658 <ofirlevy6658@gmail.com>
2021-07-28 10:18:39 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const axios = require("axios");
+const express = require("express");
+const Currency = require("./model/currency");
+
+require("./db/mongoose");
+
+const app = express();
+
+const fetchCurrency = async () => {
+	try {
+		const { data } = await axios.get(
+			"https://api.exchangerate.host/latest?base=USD"
+		);
+		return data.rates.EUR;
+	} catch (e) {
+		console.log(e.message);
+	}
+};
+
+const updateExchangeRate = () => {
+	setInterval(async () => {
+		try {
+			const newExchangeRate = await fetchCurrency();
+			const euro = await Currency.findOne({ name: "EUR" });
+			if (euro.exchangeRate === newExchangeRate) return;
+			euro.exchangeRate = newExchangeRate;
+			console.log(euro);
+			euro.save();
+		} catch (e) {
+			console.log(e.message);
+		}
+	}, 2500);
+};
+updateExchangeRate();
+
+// const curn = new Currency({
+// 	name: "EUR",
+// 	exchangeRate: 4,
+// });
+
+// curn
+// 	.save()
+// 	.then(() => {
+// 		console.log("work");
+// 	})
+// 	.catch(() => console.log("fail"));
+
+const PORT = 4000;
+app.listen(PORT, () => {
+	console.log(`listening on port ${PORT}`);
+});
